011a2378f53c57aa84afd4bdad58d8b9068849e1
sharon-sahrin <sahrinsharon@gmail.com>
2025-10-11 13:38:22 +0600

    Updated Assignment-1/Question_8.dart

---
+++ Assignment-1/Question_8.dart
@@ -0,0 +1,16 @@
+void main() {
+  var num1 = 5;
+  var num2 = 10;
+
+  print("Before swapping:");
+  print("num1 = $num1");
+  print("num2 = $num2");
+
+  var temp = num1;
+  num1 = num2;
+  num2 = temp;
+
+  print("After swapping:");
+  print("num1 = $num1");
+  print("num2 = $num2");
+}
