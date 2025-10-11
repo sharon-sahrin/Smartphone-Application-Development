b8da6838c87bb1a9079951e29e7361a3715303ef
sharon-sahrin <sahrinsharon@gmail.com>
2025-10-11 18:04:45 +0600

    Updated Assignment-1/Question_10.dart

---
+++ Assignment-1/Question_10.dart
@@ -0,0 +1,8 @@
+void main(){
+  String number1="10";
+  print("Type of number1 is ${number1.runtimeType}");
+  int number2=int.parse(number1);
+  print("Value of number2 is $number2");
+  print("Type of number2 is ${number2.runtimeType}");
+
+}
